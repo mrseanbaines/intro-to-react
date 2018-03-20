ba86c850fffc2c144b32ffe25aface72268a3e08
Sean Baines <mrseanbaines@gmail.com>
2018-03-20 09:27:56 +0000

    Revert "Pass data through props"
    This reverts commit da074a10006586ce06d8a83bbbcb1d31959dabea.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {this.props.value}
+        {/* TODO */}
       </button>
     );
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square />;
   }
 
   render() {
